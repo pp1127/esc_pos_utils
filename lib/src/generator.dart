e5069407e207b188d749934464118ecc6ee83ae8
pp1127 <73777351+pp1127@users.noreply.github.com>
2022-11-15 17:47:11 +0800

    Update generator.dart

--- lib/src/generator.dart
+++ lib/src/generator.dart
@@ -14,6 +14,7 @@ import 'package:gbk_codec/gbk_codec.dart';
 import 'package:esc_pos_utils/esc_pos_utils.dart';
 import 'enums.dart';
 import 'commands.dart';
+import 'package:dart_big5/big5.dart';
 
 class Generator {
   Generator(this._paperSize, this._profile, {this.spaceBetweenRows = 5});
@@ -76,7 +77,7 @@ class Generator {
     if (!isKanji) {
       return latin1.encode(text);
     } else {
-      return Uint8List.fromList(gbk_bytes.encode(text));
+      return Uint8List.fromList(big5.encode(text));
     }
   }
 
