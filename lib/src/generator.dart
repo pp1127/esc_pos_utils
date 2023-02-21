b42a33c22116db33f0f32b973d8f32075f390e14
pp1127 <73777351+pp1127@users.noreply.github.com>
2023-02-21 13:32:27 +0800

    Update generator.dart

--- lib/src/generator.dart
+++ lib/src/generator.dart
@@ -568,7 +568,12 @@ class Generator {
   /// Print an image using (ESC *) command
   ///
   /// [image] is an instanse of class from [Image library](https://pub.dev/packages/image)
-  List<int> image(Image imgSrc, {PosAlign align = PosAlign.center}) {
+  List<int> image(
+    Image imgSrc, {
+    PosAlign align = PosAlign.center,
+    bool highDensityHorizontal = true,
+    bool highDensityVertical = true,
+  }) {
     List<int> bytes = [];
     // Image alignment
     bytes += setStyles(PosStyles().copyWith(align: align));
